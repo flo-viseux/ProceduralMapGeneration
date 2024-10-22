a49aeedccb155257460196eb55afac584b2719ca
LAPTOP-HC1ACJB6\flori <florian.viseux@team-break.fr>
2024-10-22 17:18:59 +0200

    respect unreal convention

---
+++ MapGeneration/Source/MapGeneration/MGPointActor.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MGPointActor.h"
+
+// Sets default values
+AMGPointActor::AMGPointActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
---
+++ MapGeneration/Source/MapGeneration/MGPointActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MGPointActor.generated.h"
+
+UCLASS()
+class MAPGENERATION_API AMGPointActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMGPointActor();
+};
